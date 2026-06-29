f084c79571ba35c460f1adc189e4f776f9989287
landa <landa@users.noreply.github.com>
2026-06-29 15:26:04 +0700

    chore: cap nhat public anon key moi cho Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,7 +4,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://abfrrzuxvbnvizlwpxea.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiZnJyenV4dmJudml6bHdweGVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwMjQ0MTAsImV4cCI6MjAyNjYwMDQxMH0.Kxv3xjYv0M3EJqUqO1vR0wB_7xYnT3qVLGgR0P3QhT8';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiZnJyenV4dmJudml6bHdweGVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MTQ4MDEsImV4cCI6MjA5ODI5MDgwMX0.A9AzG5eHUseszQ_MoN0NuQSo2TX2FXmIHpeih87bdn0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
